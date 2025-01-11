7a9e7c4a0c03e5f0bc84d87549d590c4757f2bb8
edaywalid <becharwalid9@gmail.com>
2025-01-11 10:42:28 +0100

    implmented hot_reload_update to manage reload or load the target library

--- hotr.c
+++ hotr.c
@@ -1,7 +1,10 @@
 #include "hotr.h"
+#include <dlfcn.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 HotReloader *hot_reload_init(const char *lib_path) {
   HotReloader *reloader = malloc(sizeof(HotReloader));
@@ -22,4 +25,40 @@ int needs_reload(HotReloader *reloader) {
     }
   }
   return 0;
+}
+
+/*
+ * Load or reload the library
+ * 1 - Call cleanup function if it exists
+ * 2 - Close existing library
+ * 3 - Load new library
+ * 4 - Update last time modified
+ * 5 - Get cleanup function
+ */
+void *hot_reload_update(HotReloader *reloader) {
+  if (!needs_reload(reloader)) {
+    return reloader->handle;
+  }
+
+  if (reloader->cleanup != NULL) {
+    reloader->cleanup();
+  }
+
+  if (reloader->handle != NULL) {
+    dlclose(reloader->handle);
+  }
+
+  reloader->handle = dlopen(reloader->lib_path, RTLD_NOW);
+  if (!reloader->handle) {
+    fprintf(stderr, "Error loading library: %s\n", dlerror());
+    return NULL;
+  }
+
+  struct stat attr;
+  stat(reloader->lib_path, &attr);
+  reloader->last_modified = attr.st_mtime;
+
+  reloader->cleanup = dlsym(reloader->handle, "cleanup");
+
+  return reloader->handle;
 }
