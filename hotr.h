c66db0400c9f1328c58f013fac7ec17df772f363
edaywalid <becharwalid9@gmail.com>
2025-01-14 12:37:39 +0100

    added new function ( watch_for_changes )

--- hotr.h
+++ hotr.h
@@ -16,5 +16,6 @@ HotReloader *hot_reload_init(const char *lib_path);
 int needs_reload(HotReloader *reloader);
 void *hot_reload_update(HotReloader *reloader);
 void hot_reload_cleanup(HotReloader *reloader);
+int watch_for_changes(HotReloader *reloader);
 
 #endif
