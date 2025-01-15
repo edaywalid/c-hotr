6030779dacb0deb31f9576acf982ca46d73f2d84
edaywalid <becharwalid9@gmail.com>
2025-01-15 14:37:28 +0100

    added needs_reload function definition in lib file

--- hotr.h
+++ hotr.h
@@ -16,5 +16,5 @@ HotReloader *hot_reload_init(const char *lib_path);
 void *hot_reload_update(HotReloader *reloader);
 void hot_reload_cleanup(HotReloader *reloader);
 int watch_for_changes(HotReloader *reloader);
-
+int needs_reload(HotReloader *reloader);
 #endif
